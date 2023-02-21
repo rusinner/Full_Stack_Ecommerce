f9b58476acfc4b293688bc92e3f2835ed3752be3
Theologos Mavropoulos <rusinner88@gmail.com>
2023-02-21 14:07:14 +0200

    Navbar and Announcements components

---
+++ full-stack-ecommerce/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Navbar, Announcement } from "../components";
+
+const Home = () => {
+  return (
+    <div>
+      <Announcement />
+      <Navbar />
+    </div>
+  );
+};
+
+export default Home;
