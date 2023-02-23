3f8a773bc9ae06a1ce0fc08d9295f89337812527
Theologos Mavropoulos <rusinner88@gmail.com>
2023-02-23 17:16:09 +0200

    create mongodb folder

--- backend-api/index.js
+++ backend-api/index.js
@@ -1,1 +1,26 @@
+//init express app
+const express = require("express");
+const app = express();
 
+//this library allows to env variables without `${expression}`
+const dotenv = require("dotenv");
+dotenv.config();
+
+//connect to mongoose server
+const mongoose = require("mongoose");
+
+mongoose.set("strictQuery", false);
+mongoose.connect(process.env.MONGO_URL, () => {
+  console.log("Connected to MongoDB");
+});
+
+//create endpoints (routes)
+const userRoute = require("./routes/users");
+
+app.use(express.json());
+app.use("/api/users", userRoute);
+
+//connect to port
+app.listen(process.env.PORT || 5000, () => {
+  console.log("backend is running");
+});
