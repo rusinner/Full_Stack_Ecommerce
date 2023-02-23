0ea37ebb82dba30fa6f6db2e1680affa342e2058
Theologos Mavropoulos <rusinner88@gmail.com>
2023-02-23 13:26:09 +0200

    Register/Login Page

--- full-stack-ecommerce/src/pages/Product.jsx
+++ full-stack-ecommerce/src/pages/Product.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import { Navbar, Announcement, Newsletter, Footer } from "../components";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
 
 const Container = styled.div``;
 
@@ -19,13 +21,94 @@ const Image = styled.img`
   object-fit: cover;
 `;
 
-const InfoContainer = styled.div``;
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 0px 50px;
+`;
 
-const Title = styled.h1``;
+const Title = styled.h1`
+  font-weight: 200;
+`;
 
-const Description = styled.p``;
+const Description = styled.p`
+  margin: 20px 0px;
+`;
 
-const Price = styled.span``;
+const Price = styled.span`
+  font-weight: 100;
+  font-size: 40px;
+`;
+
+const FilterContainer = styled.div`
+  width: 50%;
+  margin: 30px 0px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Filter = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const FilterTitle = styled.span`
+  font-size: 20px;
+  font-weight: 200;
+`;
+
+const FilterColor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
+  margin: 0px 5px;
+  cursor: pointer;
+`;
+
+const FilterSize = styled.select`
+  margin-left: 10px;
+  padding: 5px;
+  cursor: pointer;
+`;
+
+const FilterSizeOption = styled.option``;
+
+const AddContainer = styled.div`
+  width: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const AmountContainer = styled.div`
+  display: flex;
+  align-content: center;
+  font-weight: 700;
+`;
+
+const Amount = styled.span`
+  width: 30px;
+  height: 30px;
+  border-radius: 10px;
+  border: 1px solid teal;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0px 5px;
+`;
+
+const Button = styled.button`
+  padding: 15px;
+  border: 1px solid teal;
+  background-color: white;
+  cursor: pointer;
+  transition: 0.5s ease;
+  font-weight: 600;
+  &:hover {
+    background-color: teal;
+    color: white;
+  }
+`;
 
 const Product = () => {
   return (
@@ -46,6 +129,33 @@ const Product = () => {
             perferendis. Dolores, ratione vitae!
           </Description>
           <Price>40 €</Price>
+          <FilterContainer>
+            <Filter>
+              <FilterTitle>Color</FilterTitle>
+              <FilterColor color="black" />
+              <FilterColor color="darkblue" />
+              <FilterColor color="gray" />
+            </Filter>
+            <Filter>
+              <FilterTitle>Size</FilterTitle>
+              <FilterSize>
+                <FilterSizeOption>XS</FilterSizeOption>
+                <FilterSizeOption>S</FilterSizeOption>
+                <FilterSizeOption>M</FilterSizeOption>
+                <FilterSizeOption>L</FilterSizeOption>
+                <FilterSizeOption>XL</FilterSizeOption>
+                <FilterSizeOption>XXL</FilterSizeOption>
+              </FilterSize>
+            </Filter>
+          </FilterContainer>
+          <AddContainer>
+            <AmountContainer>
+              <RemoveIcon />
+              <Amount>1</Amount>
+              <AddIcon />
+            </AmountContainer>
+            <Button>ADD TO CART</Button>
+          </AddContainer>
         </InfoContainer>
       </Wrapper>
       <Newsletter />
