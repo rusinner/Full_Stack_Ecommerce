6cc35b3397446b5eafb91915d9b58fa7f071f316
Theologos Mavropoulos <rusinner88@gmail.com>
2023-02-24 17:36:28 +0200

    sget user stats timestamp fixed

--- backend-api/models/User.js
+++ backend-api/models/User.js
@@ -7,9 +7,19 @@ const UserSchema = new mongoose.Schema(
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     isAdmin: { type: Boolean, default: false },
-  },
+
+    createdAt: {
+      type: Date,
+      required: true,
+      default: Date.now,
+    },
+    updatedAt: {
+      type: Date,
+      required: true,
+      default: Date.now,
+    },
+  }
   //timestamp creates createdAt and updatedAt on its own in mongoDB
-  { timestamp: true }
 );
 
 module.exports = mongoose.model("User", UserSchema);
