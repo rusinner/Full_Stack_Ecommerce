bc0b90791bc9e5bee9316dc46af481448337bdf4
Theologos Mavropoulos <rusinner88@gmail.com>
2023-02-24 18:30:58 +0200

    orders route

--- backend-api/models/Order.js
+++ backend-api/models/Order.js
@@ -1,26 +1,32 @@
 const mongoose = require("mongoose");
 //create schema
-const OrderSchema = new mongoose.Schema(
-  {
-    userId: { type: String, required: true },
-    products: [
-      {
-        productId: {
-          type: String,
-        },
-        quantity: {
-          type: Number,
-          default: 1,
-        },
+const OrderSchema = new mongoose.Schema({
+  userId: { type: String, required: true },
+  products: [
+    {
+      productId: {
+        type: String,
       },
-    ],
-    amount: { type: Number, required: true },
-    //address is gonna be object because Stripe after Checkout returns an object
-    adddress: { type: Object, required: true },
-    status: { type: String, default: "pending" },
+      quantity: {
+        type: Number,
+        default: 1,
+      },
+    },
+  ],
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
   },
-  //timestamp creates createdAt and updatedAt on its own in mongoDB
-  { timestamp: true }
-);
+  updatedAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+  amount: { type: Number, required: true },
+  //address is gonna be object because Stripe after Checkout returns an object
+  adddress: { type: Object, required: true },
+  status: { type: String, default: "pending" },
+});
 
 module.exports = mongoose.model("Order", OrderSchema);
