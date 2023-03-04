47526b52027ae41e5fe259f7f8dad30b72d2af3f
Theologos Mavropoulos <rusinner88@gmail.com>
2023-03-04 12:26:30 +0200

    TopBar

--- admin/src/components/topbar/Topbar.jsx
+++ admin/src/components/topbar/Topbar.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./topbar.css";
 import NotificationsIcon from "@mui/icons-material/Notifications";
+import LanguageIcon from "@mui/icons-material/Language";
+import SettingsIcon from "@mui/icons-material/Settings";
 
 const Topbar = () => {
   return (
@@ -14,6 +16,18 @@ const Topbar = () => {
             <NotificationsIcon />
             <span className="topIconBadge">2</span>
           </div>
+          <div className="topbarIconContainer">
+            <LanguageIcon />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <SettingsIcon />
+          </div>
+          <img
+            src="https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375_960_720.png"
+            alt="profile pic"
+            className="topAvatar"
+          />
         </div>
       </div>
     </div>
