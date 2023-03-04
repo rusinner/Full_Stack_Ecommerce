5081502073fc8fe5f7646b1665269e97ce1023e4
Theologos Mavropoulos <rusinner88@gmail.com>
2023-03-04 14:13:50 +0200

    chart

---
+++ admin/src/components/paskooloGraph/dummyData.js
@@ -0,0 +1,50 @@
+export const userData = [
+  {
+    name: "Jan",
+    ActiveUser: 4000,
+  },
+  {
+    name: "Feb",
+    ActiveUser: 3000,
+  },
+  {
+    name: "Mar",
+    ActiveUser: 6000,
+  },
+  {
+    name: "Apr",
+    ActiveUser: 2000,
+  },
+  {
+    name: "May",
+    ActiveUser: 3000,
+  },
+  {
+    name: "Jun",
+    ActiveUser: 4000,
+  },
+  {
+    name: "Jul",
+    ActiveUser: 8000,
+  },
+  {
+    name: "Aug",
+    ActiveUser: 6000,
+  },
+  {
+    name: "Sep",
+    ActiveUser: 5000,
+  },
+  {
+    name: "Oct",
+    ActiveUser: 3000,
+  },
+  {
+    name: "Nov",
+    ActiveUser: 4000,
+  },
+  {
+    name: "Dec",
+    ActiveUser: 9000,
+  },
+];
--- admin/src/pages/home/Home.jsx
+++ admin/src/pages/home/Home.jsx
@@ -1,11 +1,19 @@
 import React from "react";
-import { FeaturedInfo } from "../../components";
+import { FeaturedInfo, PaskooloGraph } from "../../components";
+import { userData } from "../../components/paskooloGraph/dummyData";
 import "./home.css";
 
 const Home = () => {
   return (
     <div className="home">
       <FeaturedInfo />
+      <PaskooloGraph
+        data={userData}
+        title="User Analytics"
+        grid
+        dataKey="ActiveUser"
+      />
+      <div className="homeWidgets"></div>
     </div>
   );
 };
