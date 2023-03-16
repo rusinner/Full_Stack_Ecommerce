f3e683b8aaa571c61b1b48f1f7770124a7703f11
Theologos Mavropoulos <rusinner88@gmail.com>
2023-03-16 11:53:01 +0200

    Login page

--- admin/src/pages/login/Login.jsx
+++ admin/src/pages/login/Login.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
-  return <div></div>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const handleClick = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="username"
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleClick}>Login</button>
+    </div>
+  );
 };
 
 export default Login;
